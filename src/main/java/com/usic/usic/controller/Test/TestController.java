74d8465a507fabdaa267f0db98a9f6e7a87e4180
javierfc16 <javierfc1611@gmail.com>
2024-09-13 15:30:27 -0400

    hhh

--- src/main/java/com/usic/usic/controller/Test/TestController.java
+++ src/main/java/com/usic/usic/controller/Test/TestController.java
@@ -2,14 +2,19 @@ package com.usic.usic.controller.Test;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.usic.usic.model.Entity.Persona;
 
 @Controller
 public class TestController {
     
     @GetMapping(value = "/vista-test")
-    public String vistaPersona(Model model) {
+    public String vistaPersona(@Validated Persona persona, Model model) {
         
+        model.addAttribute("persona", new Persona());
+
         return "test/vista_registro1";
     }
 }
--- src/main/java/com/usic/usic/controller/estudiante/EstudianteController.java
+++ src/main/java/com/usic/usic/controller/estudiante/EstudianteController.java
@@ -5,6 +5,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -143,6 +144,17 @@ public class EstudianteController {
         }
 
         return "redirect:/vista-estudiantes";
+    }
+
+    /*CRISTIANA*/
+    @PostMapping(value = "/guardar_estudiante_inicio")
+    public String guardar_estudiante_inicio(@Validated Persona persona, Model model) {
+
+        
+
+        personaService.save(persona);
+        
+        return "redirect:/vista-test";
     }
 
     
