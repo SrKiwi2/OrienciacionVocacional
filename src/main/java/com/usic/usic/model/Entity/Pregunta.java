37decce8220950fb604cc9c09a131fd46fc027be
SrKiwi2 <kcallisaya2.0@gmail.com>
2024-09-16 17:01:23 -0400

    Kevin_16092024_1701

--- src/main/java/com/usic/usic/model/Entity/Pregunta.java
+++ src/main/java/com/usic/usic/model/Entity/Pregunta.java
@@ -28,4 +28,8 @@ public class Pregunta extends AuditoriaConfig{
     @ManyToOne
     @JoinColumn(name = "id_tipo_test")
     private TipoTest tipoTest;
+
+    @ManyToOne
+    @JoinColumn(name = "id_tipo_pregunta")
+    private TipoPregunta tipoPregunta;
 }
