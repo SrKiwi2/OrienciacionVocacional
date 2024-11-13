49c5102144488138671913b0b4a39fae6f6f3877
SrKiwi2 <kcallisaya2.0@gmail.com>
2024-11-13 17:03:28 -0400

    Kevin_13102024_1703

--- src/main/java/com/usic/usic/controller/API/ApiPersona.java
+++ src/main/java/com/usic/usic/controller/API/ApiPersona.java
@@ -11,6 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.usic.usic.model.DTO.PersonaDTO;
+import com.usic.usic.model.Entity.Estudiante;
+import com.usic.usic.model.Entity.Persona;
+import com.usic.usic.model.Service.IEstudianteService;
 import com.usic.usic.model.Service.IPersonaService;
 
 import lombok.RequiredArgsConstructor;
@@ -20,19 +23,30 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public class ApiPersona {
     private final IPersonaService personaService;
+    private final IEstudianteService estudianteService;
 
     private static final String API_KEY = "7fd4232f2344fw5423234214usic";
 
     //API PARA MANDAR DATOS DE ESTUDINATES ATRAVEZ DEL CI
     @PostMapping("/buscarPorCi")
-    public ResponseEntity<PersonaDTO> obtenerPersonaPorCi(@RequestHeader(value = "API-Key", required = true) String apiKey,@RequestBody Map<String, Object> payload) {
-
+    public ResponseEntity<?> obtenerPersonaPorCi(@RequestHeader(value = "API-Key", required = true) String apiKey,@RequestBody Map<String, Object> payload) {
+        
         if (!API_KEY.equals(apiKey)) {
             return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
         }
 
         String ci = payload.get("ci").toString();
+
+        if (personaService.validarCI(ci) == null) {
+            return ResponseEntity.status(HttpStatus.NOT_FOUND)
+                             .body("El CI proporcionado no corresponde a ningún estudiante registrado.");
+        }
+        
+        Persona persona_encontrada = personaService.validarCI(ci);
+        Estudiante estudiante = estudianteService.findByPersona(persona_encontrada);
+
         PersonaDTO personaDTO = personaService.obtenerPersonaPorCi(ci);
+        personaDTO.setUrl_certificado("http://virtual.uap.edu.bo:9597/reporte/pdf/"+estudiante.getIdEstudiante());
         return ResponseEntity.ok(personaDTO);
     }
 }
